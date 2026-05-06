6edfd47b39adcb796b15e83ea98b06ed31baa9d2
qqzzaw <xiat87898@gmail.com>
2026-05-06 17:58:05 +0900

    Update app.py

--- app.py
+++ app.py
@@ -109,5 +109,8 @@ def on_action(data):
     pass
 
 if __name__ == '__main__':
+    # 关键：必须读取环境变量 PORT
+    import os
     port = int(os.environ.get("PORT", 5000))
     socketio.run(app, host='0.0.0.0', port=port)
+
